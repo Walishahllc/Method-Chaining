7350a99595be6d8c8eb4bbbe478c36ba387929df
Wali Shah <walishahllc@gmail.com>
2024-06-28 16:40:23 +0500

    Chaining

---
+++ app.js
@@ -0,0 +1,9 @@
+let work = "  Web developer";
+// let newwork = work.trim();
+// console.log("After Trim", newwork);
+// newwork = newwork.toUpperCase();
+// console.log("After Uppercase", newwork);
+
+// Its called method chaining
+let newwork = work.trim().toUpperCase();
+console.log(newwork);
